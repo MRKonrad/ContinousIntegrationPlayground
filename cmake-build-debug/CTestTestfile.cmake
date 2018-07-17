37be86b1ead7d77a322f3ed86e13bad3fcdc148d
Konrad Werys <konradwerys2@gmail.com>
2018-07-17 13:21:04 +0100

    simlifying

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/kwerys/Code/ContinousIntegrationPlayground
+# Build directory: /Users/kwerys/Code/ContinousIntegrationPlayground/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
