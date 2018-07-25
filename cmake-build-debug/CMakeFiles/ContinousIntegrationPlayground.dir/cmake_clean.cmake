63d68b62da15d6b74396edcdbd4473a5153fcc94
Konrad Werys <konradwerys2@gmail.com>
2018-07-25 21:35:19 +0100

    Clean gh-pages branch

---
+++ cmake-build-debug/CMakeFiles/ContinousIntegrationPlayground.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ContinousIntegrationPlayground.dir/main.cpp.o"
+  "ContinousIntegrationPlayground.pdb"
+  "ContinousIntegrationPlayground"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ContinousIntegrationPlayground.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ContinousIntegrationPlayground.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ContinousIntegrationPlayground.
+# This may be replaced when dependencies are built.
