304da03392a58a9b7eebb61f4700673750831b8b
Konrad Werys <konradwerys2@gmail.com>
2018-07-26 21:24:07 +0100

    deploy. take 1

---
+++ lib/mylib2.h
@@ -0,0 +1,21 @@
+/*!
+ * \file mylib2.h
+ * \brief A Documented file.
+ * \details Details of the file should be here. LOL
+ * \author Konrad Werys
+ * \date 2018/08/24
+ */
+
+#ifndef CONTINOUSINTEGRATIONPLAYGROUND_MYLIB2_H
+#define CONTINOUSINTEGRATIONPLAYGROUND_MYLIB2_H
+
+/**
+ * \brief a dummy function that returns the output integer
+ * @param input
+ * @return input
+ */
+int dummyFunction2(int input){
+    return input;
+}
+
+#endif //CONTINOUSINTEGRATIONPLAYGROUND_MYLIB2_H
